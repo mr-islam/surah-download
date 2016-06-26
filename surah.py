2c86211be4a95dff1bb75e6692357b706115fdc8
NavedCoded <navecoded@gmail.com>
2016-06-26 16:08:06 +0100

    non-working, but progress

--- surah.py
+++ surah.py
@@ -1,8 +1,17 @@
 from requests import get
 
+reciter = "mishaari_raashid_al_3afaasee"
+surah_number = 1 
+surah_url = str(surah_number).zfill(3)
+
+#url = "http://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3"
+url = "http://download.quranicaudio.com/quran/" + reciter + "/" + surah_url +  ".mp3"
+"""
 def download(url, file_name):
     with open(file_name, "wb") as file:
         response = get(url)
         file.write(response.content)
-
-
+"""
+for i in range(3):
+    print(url)
+    str(int(surah_number) + 1)
