279d7578c973b7a82f64e7523a1ea3858b1fb714
NavedCoded <navecoded@gmail.com>
2016-06-26 17:55:52 +0100

    Awesomely shaping up. A milestone.

--- surah.py
+++ surah.py
@@ -1,11 +1,23 @@
 from requests import get
 
-reciter = "mishaari_raashid_al_3afaasee"
-#surah_url = str(surah_number).zfill(3)
+q_reciter = input('Which reciter would you like to listen to? \n 1 - Mishary Rashid Al \'Afasy \n 2 - [coming soon!] \n')
 
-#url = "http://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3"
+if q_reciter == 1:
+    reciter = "mishaari_raashid_al_3afaasee"
+##elif q_reciter == 2:
+##    other reciter blablabla
+else:
+    reciter = "mishaari_raashid_al_3afaasee"
+
+q_surahs = input('Would you like to download all the Surahs? \n 1 - Yes, all of them! \n 2 - [coming soon] \n')
+
+##if q_surahs == 1:
+##    #bla
+##else if q_surahs == 2:
+##    #bla
 
 surah_number = 1 
+#surah_url = str(surah_number).zfill(3)
 for i in range(3):
     url = "http://download.quranicaudio.com/quran/" + reciter + "/" + str(surah_number) +  ".mp3"
     print(url)
@@ -19,3 +31,6 @@ def download(url, file_name):
         file.write(response.content)
 
 """
+#TODO: option, number of surahs to download, or all - make it branches with prompts
+#TODO: choice of reciter
+#TODO: creat vars.py with name of sirah, to choose one suah to dowwnload, and also filenames
