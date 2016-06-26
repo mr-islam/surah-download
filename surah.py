6ee4f199b215320d4854a671c26a40fdcbdc670a
NavedCoded <navecoded@gmail.com>
2016-06-26 17:11:00 +0100

    Finally it increments! Huzzah! Lots to do still...

--- surah.py
+++ surah.py
@@ -1,17 +1,21 @@
 from requests import get
 
 reciter = "mishaari_raashid_al_3afaasee"
-surah_number = 1 
-surah_url = str(surah_number).zfill(3)
+#surah_url = str(surah_number).zfill(3)
 
 #url = "http://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3"
-url = "http://download.quranicaudio.com/quran/" + reciter + "/" + surah_url +  ".mp3"
+
+surah_number = 1 
+for i in range(3):
+    url = "http://download.quranicaudio.com/quran/" + reciter + "/" + str(surah_number) +  ".mp3"
+    print(url)
+    int(surah_number)
+    surah_number += 1
+    str(surah_number)
 """
 def download(url, file_name):
     with open(file_name, "wb") as file:
         response = get(url)
         file.write(response.content)
+
 """
-for i in range(3):
-    print(url)
-    str(int(surah_number) + 1)
